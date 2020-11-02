1c27a9de36cb41a3d75f04713253d5385885f838
Chinmay Navale <chinmaymnavale@gmail.com>
2020-11-02 15:32:32 +0530

    Implemented CRUD operations on our Genres list api endpoint

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
 const genres = [
   { id: 1, name: 'Action' },
   { id: 2, name: 'Horror' },
@@ -8,6 +10,30 @@ const genres = [
 ];
 
 app.get('/api/genres', (req, res) => res.send(genres));
+
+app.post('/api/genres', (req, res) => {
+  const genre = {
+    id: genres[genres.length - 1].id + 1,
+    name: req.body.name,
+  };
+  genres.push(genre);
+
+  res.send(genre);
+});
+
+app.put('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  genre.name = req.body.name;
+
+  res.send(genres);
+});
+
+app.delete('/api/genres/:id', (req, res) => {
+  const genre = genres.find(g => g.id === parseInt(req.params.id));
+  genres.splice(genres.indexOf(genre), 1);
+
+  res.send(genres);
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
